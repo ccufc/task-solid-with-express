3868779464f9b232b61856d845f0c7e0ccc6c084
EmanoelVitor <emanuelvitor575@gmail.com>
2024-09-02 14:33:23 -0300

    style: rename 'createUserusecase' to 'createUserUseCase'

--- src/controllers/create-user-controller/index.ts
+++ src/controllers/create-user-controller/index.ts
@@ -1,9 +1,9 @@
 import { CreateUserController } from './create-user-controller'
-import { CreateUserUsecase } from '@/use-cases/create-user-use-case'
+import { CreateUserUseCase } from '@/use-cases/create-user-use-case'
 import { SqliteUserRepository } from '@/repositories/sqlite-user-repository'
 
 const sqliteUserRepository = new SqliteUserRepository()
-const createUserUsecase = new CreateUserUsecase(sqliteUserRepository)
+const createUserUsecase = new CreateUserUseCase(sqliteUserRepository)
 const createUserController = new CreateUserController(createUserUsecase)
 
 export default createUserController
--- src/use-cases/create-user-use-case.ts
+++ src/use-cases/create-user-use-case.ts
@@ -2,7 +2,7 @@ import type { User } from '@/entities/user'
 import type { ICreateUserUseCase } from '@/interfaces/create-user-use-case-interface'
 import type { IUserRepository } from '@/interfaces/user-repository-interface'
 
-export class CreateUserUsecase implements ICreateUserUseCase {
+export class CreateUserUseCase implements ICreateUserUseCase {
   constructor(private readonly repository: IUserRepository) {}
 
   public async execute(user: User): Promise<void> {
