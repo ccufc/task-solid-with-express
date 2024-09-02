790e8ad46e287ee809b123b22a5d866b57ebeed3
EmanoelVitor <emanuelvitor575@gmail.com>
2024-09-02 14:30:48 -0300

    feat: add get users mapper

--- src/controllers/get-users-controller/get-users-controller.ts
+++ src/controllers/get-users-controller/get-users-controller.ts
@@ -1,15 +1,17 @@
 import type { Response, Request } from 'express'
 import type { IGetUsersUseCase } from '@/interfaces/get-users-use-case-interface'
 import type { IGetUsersController } from '@/interfaces/get-users-controller-interface'
+import GetUsersMapper from '@/mappers/get-users-mapper'
 
 export class GetUsersController implements IGetUsersController {
   constructor(private readonly getUsersUseCase: IGetUsersUseCase) {}
 
   async handle(request: Request, response: Response): Promise<Response> {
     try {
-      const users = await this.getUsersUseCase.execute()
+      const data = await this.getUsersUseCase.execute()
+      const users = GetUsersMapper.mapTo(data)
       return response.status(200).json(users)
-    } catch (error) {
+    } catch {
       return response.status(500).json({ error: 'failed to retrive users' })
     }
   }
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import app from './app'
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`)
